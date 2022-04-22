bb56c09c58120f022c6531a89b2e0dc85c327391
Niro Lin <linqinyun@outlook.com>
2022-04-21 21:03:51 -0400

    reimplement model fitting with ML Pipeline

--- test/spark/FitModelTest.scala
+++ test/spark/FitModelTest.scala
@@ -1,12 +1,13 @@
 package spark
 
-import org.apache.spark.ml.classification.{LogisticRegression, RandomForestClassifier}
+import org.apache.spark.ml.PipelineModel
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{count, when}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.tagobjects.Slow
 import play.api.libs.json.{JsValue, Json}
+import spark.FitModel.assembleScalePipeline
 
 import scala.util.Success
 
@@ -35,7 +36,7 @@ class FitModelTest extends AnyFlatSpec with Matchers{
     val filepath = getClass.getResource("/sample_songs.csv").getPath
 
     val try_processed_df = for(df <- FitModel.loadCsv(filepath, spark);
-                               pdf <- FitModel.preprocessing(df)) yield pdf
+                               pdf <- FitModel.columnProcessing(df)) yield pdf
 
     try_processed_df should matchPattern {
       case Success(_) =>
@@ -46,7 +47,12 @@ class FitModelTest extends AnyFlatSpec with Matchers{
     val popular_songs_num = processed_df.select(count(when(processed_df("label")===1, 1))).first().getLong(0)
     popular_songs_num shouldBe 454
 
-    val features_num = processed_df.select("features").take(1)(0)(0).toString.split(",").length
+    val columns_num = processed_df.columns.length
+    columns_num shouldBe 27
+
+    val assemble_scale_pipeline_model = assembleScalePipeline(processed_df).fit(processed_df)
+    val transformed_df = assemble_scale_pipeline_model.transform(processed_df)
+    val features_num = transformed_df.select("features").take(1)(0)(0).toString.split(",").length
     features_num shouldBe 18
   }
 
@@ -55,7 +61,7 @@ class FitModelTest extends AnyFlatSpec with Matchers{
 
     val try_models = for (
       raw_df <- FitModel.loadCsv(filepath, spark);
-      processed_df <- FitModel.preprocessing(raw_df)
+      processed_df <- FitModel.columnProcessing(raw_df)
     ) yield (
       FitModel.fit(processed_df, FitModel.ModelName_LR, evaluate = true),
       FitModel.fit(processed_df, FitModel.ModelName_RF, evaluate = true)
@@ -63,6 +69,17 @@ class FitModelTest extends AnyFlatSpec with Matchers{
 
     try_models should matchPattern {
       case Success((Success(_), Success(_))) =>
+    }
+
+    try_models match {
+      case Success((lrTry, rfTry)) => (lrTry, rfTry) match {
+        case (Success(lr: PipelineModel), Success(rf: PipelineModel)) =>
+          val df = FitModel.columnProcessing(FitModel.loadCsv(filepath, spark).get.limit(100)).get
+          lr.transform(df).select("prediction")
+            .head(3).map(r => r.getDouble(0)) shouldBe Array(1.0, 1.0, 0.0)
+          rf.transform(df).select("prediction")
+            .head(3).map(r => r.getDouble(0)) shouldBe Array(1.0, 1.0, 0.0)
+      }
     }
   }
 
